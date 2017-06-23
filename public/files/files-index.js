cd8f062cfafff3768f9c5b3fe0c5dd01ac300aeb
djx314 <djx314@sina.cn>
2017-06-24 02:11:37 +0800

    Use ko.

--- public/files/files-index.js
+++ public/files/files-index.js
@@ -1,12 +1,23 @@
 $(function() {
 
+    var ViewModel = function() {
+        var self = this;
+        self.parentUrl = ko.observable(null);
+        self.filesPath = ko.observableArray([]);
+    };
+
+    var viewModel = new ViewModel();
+
+    ko.applyBindings(viewModel);
+
     $.ajax({
         type: "POST",
         url: "/dirInfoRequest",
         dataType: "json",
         data: { path: currentUrl }
     }).done(function(response) {
-        console.log(response);
+        viewModel.parentUrl(response.parentPath);
+        viewModel.filesPath(response.urls);
     });
 
 });
